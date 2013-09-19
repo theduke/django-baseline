e8831317ad155037ca8ccb728a0b580a2f5795e9
Christoph Herzog <chris@theduke.at>
2013-09-19 19:35:04 +0200

    Added multi template support for render_template()

--- template.py
+++ template.py
@@ -1,10 +1,22 @@
 
 from django import template
 
-def render_template(tpl_name, context):
+def render_template(tpl, context):
 	'''
 	A shortcut function to render a partial template with context and return 
 	the output.
 	'''
-	t = template.loader.get_template(tpl_name)
-	return t.render(template.Context(context))
+
+	templates = [tpl] if type(tpl) == str else tpl
+	tpl_instance = None
+
+	for tpl in templates:
+		try:
+			tpl_instance = template.loader.get_template(tpl)
+		except template.TemplateDoesNotExist:
+			pass
+
+	if not tpl_instance:
+		raise Exception('Template does not exist: ' + templates[-1])
+
+	return tpl_instance.render(template.Context(context))
