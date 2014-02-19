bf05e93a565942fed898f4916a06993c97a2f30d
theduke <Christoph Herzog>
2014-02-19 17:24:02 +0100

    Added a resolve_class helper function for getting a class object based on absolute classpath

--- __init__.py
+++ __init__.py
@@ -4,3 +4,13 @@ from django.core.urlresolvers import reverse
 # Workaround for using reverse with success_url in class based generic views
 # because direct usage of it throws an exception.
 reverse_lazy = lambda name=None, *args : lazy(reverse, str)(name, args=args)
+
+
+def resolve_class(class_path):
+    """
+    Load a class by a fully qualified class_path,
+    eg. myapp.models.ModelName
+    """
+    modulepath, classname = class_path.rsplit('.', 1)
+    module = __import__(modulepath, fromlist=[classname])
+    return getattr(module, classname)
