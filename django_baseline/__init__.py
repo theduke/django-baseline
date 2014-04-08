6fcff882f7aab0e8feae23a0ca8cf84a91937900
theduke <chris@theduke.at>
2014-04-09 01:21:07 +0200

    Added convenience function for retrieving csrf token

--- django_baseline/__init__.py
+++ django_baseline/__init__.py
@@ -7,6 +7,16 @@ from django.utils.functional import lazy
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import Group
 from django.conf import settings
+from django.middleware import csrf
+
+
+def get_or_create_csrf_token(request):
+    token = request.META.get('CSRF_COOKIE', None)
+    if token is None:
+        token = csrf._get_new_csrf_key()
+        request.META['CSRF_COOKIE'] = token
+        request.META['CSRF_COOKIE_USED'] = True
+    return token
 
 
 def get_config(key, default=None):
